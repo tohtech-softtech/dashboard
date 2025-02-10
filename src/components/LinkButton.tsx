71c73fb4e9aa12e42662833585a1e12d2b560434
lag129 <65485662+lag129@users.noreply.github.com>
2025-02-10 23:56:19 +0900

    LinkButtonを追加

---
+++ src/components/LinkButton.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+interface LinkButtonProps {
+  href: string;
+  imgUrl: string;
+  children: ReactNode;
+}
+
+export const LinkButton: React.FC<LinkButtonProps> = ({ href, imgUrl, children }) => {
+  return (
+    <div>
+      <Link href={href} className="btn">
+        <Image src={imgUrl} alt="" width={16} height={16} />
+        {children}
+      </Link>
+    </div>
+  );
+};
