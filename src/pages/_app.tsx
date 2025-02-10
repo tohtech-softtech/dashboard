6b23f2c3274315a03d5d424b12254bafe73cc363
lag129 <65485662+lag129@users.noreply.github.com>
2025-02-10 22:46:13 +0900

    初期設定

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Noto_Sans_JP } from "next/font/google";
+
+const notoSans = Noto_Sans_JP({
+  variable: "--font-noto-sans",
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={notoSans.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
