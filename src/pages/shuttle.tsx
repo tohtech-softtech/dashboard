d71bd9f36c5ffe78cde9bff30da751b45bb53282
lag129 <65485662+lag129@users.noreply.github.com>
2025-02-22 17:25:07 +0900

    時刻やタイプ表示の部分を修正

--- src/pages/shuttle.tsx
+++ src/pages/shuttle.tsx
@@ -51,12 +51,13 @@ export default function TimeTable() {
           <div key={item.id} className="card grid grid-cols-2 bg-gray-100 p-4">
             <div className="text-left">
               <div className="text-base font-medium">{item.destination}</div>
-              <div>
-                {item.bus_type} {item.time_required}分
+              <div className="flex grid-cols-2 gap-2">
+                {item.bus_type === "" ? null : <div>{item.bus_type}</div>}
+                <div>{item.time_required}分</div>
               </div>
             </div>
             <div className="flex items-center justify-end text-xl font-bold">
-              {item.departure_hour}:{item.departure_minute}
+              {item.departure_hour}:{item.departure_minute === 0 ? "00" : item.departure_minute}
             </div>
           </div>
         ))}
