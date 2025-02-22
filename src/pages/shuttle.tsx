f93c1719a98ccaf3c177f3c25e61fb38893f2559
lag129 <65485662+lag129@users.noreply.github.com>
2025-02-22 16:36:48 +0900

    ボトムナビゲーションを追加

--- src/pages/shuttle.tsx
+++ src/pages/shuttle.tsx
@@ -27,18 +27,33 @@ export default function TimeTable() {
   }, []);
 
   return (
-    <div className="grid grid-cols-1 gap-4 p-8">
+    <>
+      <div className="grid grid-cols-1 gap-4 p-8 pb-20 ">
         {timeTable.map((item) => (
           <div key={item.id} className="card grid grid-cols-2 bg-gray-100 p-4">
             <div className="text-left">
               <div className="text-base font-medium">{item.destination}</div>
-              <div>{item.bus_type} {item.time_required}分</div>
+              <div>
+                {item.bus_type} {item.time_required}分
+              </div>
             </div>
             <div className="flex items-center justify-end text-xl font-bold">
               {item.departure_hour}:{item.departure_minute}
             </div>
           </div>
         ))}
-    </div>
+      </div>
+      <div className="btm-nav">
+        <button type="button">
+          <span className="btm-nav-label text-xs font-bold">八木山キャンパス</span>
+        </button>
+        <button type="button" className="active">
+          <span className="btm-nav-label text-xs font-bold">長町キャンパス</span>
+        </button>
+        <button type="button">
+          <span className="btm-nav-label text-xs font-bold">八木山動物公園駅</span>
+        </button>
+      </div>
+    </>
   );
 }
