d5c2f19a45225758e54725a2dca8c97a53dcd038
lag129 <65485662+lag129@users.noreply.github.com>
2025-02-22 19:43:23 +0900

    フォントサイズの変更とDividerを追加

--- src/pages/shuttle.tsx
+++ src/pages/shuttle.tsx
@@ -40,25 +40,33 @@ export default function TimeTable() {
     };
     window.scrollTo({
       top: 0,
-      behavior: "smooth"
+      behavior: "smooth",
     });
     loadData();
   }, [location]);
 
   return (
     <>
-      <div className="grid grid-cols-1 gap-4 p-8 pb-20 ">
-        <Divider>シャトルバス</Divider>
+      <div className="grid grid-cols-1 gap-4 p-8 pb-24 ">
+        <Divider>
+          {
+            {
+              yagiyama: "八木山キャンパス発 シャトルバス",
+              nagamachi: "長町キャンパス発 シャトルバス",
+              station: "八木山動物公園駅発 シャトルバス",
+            }[location]
+          }
+        </Divider>
         {timeTable.map((item) => (
           <div key={item.id} className="card grid grid-cols-2 bg-gray-100 p-4">
             <div className="text-left">
-              <div className="text-base font-semibold">{item.destination} 行</div>
-              <div className="flex grid-cols-2 gap-2 text-sm font-semibold">
+              <div className="text-base font-bold">{item.destination} 行</div>
+              <div className="flex grid-cols-2 gap-2 text-sm font-bold">
                 {item.bus_type === "" ? null : <div>{item.bus_type}</div>}
                 <div>{item.time_required}分</div>
               </div>
             </div>
-            <div className="flex items-center justify-end text-xl font-bold">
+            <div className="flex items-center justify-end text-xl font-extrabold">
               {item.departure_hour}:{item.departure_minute === 0 ? "00" : item.departure_minute}
             </div>
           </div>
@@ -66,13 +74,13 @@ export default function TimeTable() {
       </div>
       <div className="btm-nav">
         <button type="button" className={location === "yagiyama" ? "active" : ""} onClick={() => setLocation("yagiyama")}>
-          <span className="btm-nav-label text-xs font-bold">八木山キャンパス 発</span>
+          <span className="btm-nav-label text-xs font-extrabold">八木山キャンパス 発</span>
         </button>
         <button type="button" className={location === "nagamachi" ? "active" : ""} onClick={() => setLocation("nagamachi")}>
-          <span className="btm-nav-label text-xs font-bold">長町キャンパス 発</span>
+          <span className="btm-nav-label text-xs font-extrabold">長町キャンパス 発</span>
         </button>
         <button type="button" className={location === "station" ? "active" : ""} onClick={() => setLocation("station")}>
-          <span className="btm-nav-label text-xs font-bold">八木山動物公園駅 発</span>
+          <span className="btm-nav-label text-xs font-extrabold">八木山動物公園駅 発</span>
         </button>
       </div>
     </>
